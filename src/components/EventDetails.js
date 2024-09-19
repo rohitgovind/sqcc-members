1ec0a8aea23ba527d28943274521fe8846e4e446
rohitgovind <rohitgovind2204@gmail.com>
2024-09-19 22:41:24 +0530

    first commit

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -7,23 +7,23 @@ const EventDetails = () => {
         <h1 className="flex text-[#F0286A] font-bold text-5xl lg:text-6xl justify-center text-shadow my-10">MENTORS</h1>
         <div className="flex flex-wrap justify-center gap-10 my-10 w-[80vw]">
           <div className="w-[250px] h-[300px] hover:bg-mentor-card rounded-xl p-5 hover:shadow-[0_0_10px_1px_rgba(0,0,0)] flex flex-col justify-center items-center duration-150">
-            <img className="w-[150px] h-[175px] object-cover rounded-lg duration-200" src=".." alt="image here" />
+            <img className="w-[150px] h-[175px] object-cover rounded-lg duration-200" src="#" alt="image here" />
             <h1 className="mt-5 text-black text-center text-2xl font-medium">Student name</h1>
           </div>
           <div className="w-[250px] h-[300px] hover:bg-mentor-card rounded-xl p-5 hover:shadow-[0_0_10px_1px_rgba(0,0,0)] flex flex-col justify-center items-center duration-150">
-            <img className="w-[150px] h-[175px] object-cover rounded-lg duration-200" src=".." alt="image here" />
+            <img className="w-[150px] h-[175px] object-cover rounded-lg duration-200" src="#" alt="image here" />
             <h1 className="mt-5 text-black text-center text-2xl font-medium">Student name</h1>
           </div>
           <div className="w-[250px] h-[300px] hover:bg-mentor-card rounded-xl p-5 hover:shadow-[0_0_10px_1px_rgba(0,0,0)] flex flex-col justify-center items-center duration-150">
-            <img className="w-[150px] h-[175px] object-cover rounded-lg duration-200" src=".." alt="image here" />
+            <img className="w-[150px] h-[175px] object-cover rounded-lg duration-200" src="#" alt="image here" />
             <h1 className="mt-5 text-black text-center text-2xl font-medium">Student name</h1>
           </div>
           <div className="w-[250px] h-[300px] hover:bg-mentor-card rounded-xl p-5 hover:shadow-[0_0_10px_1px_rgba(0,0,0)] flex flex-col justify-center items-center duration-150">
-            <img className="w-[150px] h-[175px] object-cover rounded-lg duration-200" src=".." alt="image here" />
+            <img className="w-[150px] h-[175px] object-cover rounded-lg duration-200" src="#" alt="image here" />
             <h1 className="mt-5 text-black text-center text-2xl font-medium">Student name</h1>
           </div>
           <div className="w-[250px] h-[300px] hover:bg-mentor-card rounded-xl p-5 hover:shadow-[0_0_10px_1px_rgba(0,0,0)] flex flex-col justify-center items-center duration-150">
-            <img className="w-[150px] h-[175px] object-cover rounded-lg duration-200" src=".." alt="image here" />
+            <img className="w-[150px] h-[175px] object-cover rounded-lg duration-200" src="#" alt="image here" />
             <h1 className="mt-5 text-black text-center text-2xl font-medium">Student name</h1>
           </div>
         </div>
